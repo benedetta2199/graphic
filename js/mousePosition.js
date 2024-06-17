0250458face3963077aa82098057d4f35a8ffbdd
benedetta.2199 <benedetta.bottari@studio.unibo.it>
2024-06-17 15:19:26 +0200

    ok

--- js/mousePosition.js
+++ js/mousePosition.js
@@ -52,11 +52,11 @@ function clamp(value, min, max) {
  * @param {KeyboardEvent} event - Evento keydown
  */
 export function handleKeyDown(event) {
-    const passo = 3;
+    const passo = 25;
     if (event.key === "ArrowUp") {
-      mouseY =  Math.max(100, mouseY-3); // Incrementa la posizione Y
+      mouseY =  Math.max(100, mouseY-passo); // Incrementa la posizione Y
     } else if (event.key === "ArrowDown") {
-      mouseY = Math.min(window.innerHeight-100, mouseY+3); // Decrementa la posizione Y
+      mouseY = Math.min(window.innerHeight-100, mouseY+passo); // Decrementa la posizione Y
     }
     //mouseY = clamp(mouseY, -57, 35); // Limita la posizione Y
 }
