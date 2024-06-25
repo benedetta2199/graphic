8ab815be82dc77611b3f195ec9c1536d12fd9413
benedetta.2199 <benedetta.bottari@studio.unibo.it>
2024-06-25 13:07:38 +0200

    fixed renderObjEnd e renderScene

--- js/renderObjEnd.js
+++ js/renderObjEnd.js
@@ -1,72 +1,87 @@
 "use strict";
 
-import { posRelX} from './mousePosition.js';
+import { posRelX } from './mousePosition.js';
 import { degToRad } from './utils.js';
 
-export function renderObj(gl,meshProgramInfo, part, u_world, color =null) {
+/**
+ * Render an object part with the specified world transformation matrix.
+ * @param {WebGLRenderingContext} gl - WebGL context.
+ * @param {Object} meshProgramInfo - Shader program information.
+ * @param {Array} part - Object part to render.
+ * @param {Object} u_world - World transformation matrix.
+ */
+export function renderObj(gl, meshProgramInfo, part, u_world) {
   for (const { bufferInfo, material } of part) {
     webglUtils.setBuffersAndAttributes(gl, meshProgramInfo, bufferInfo);
-    /*if(color){
-      webglUtils.setUniforms(meshProgramInfo, { u_world: u_world, u_color: material.diffuse }, material);
-  
-    }else{
-      webglUtils.setUniforms(meshProgramInfo, { u_world: u_world }, material);
-    }*/
-      webglUtils.setUniforms(meshProgramInfo, { u_world: u_world }, material);
+    webglUtils.setUniforms(meshProgramInfo, { u_world }, material);
     webglUtils.drawBufferInfo(gl, bufferInfo);
   }
 }
 
-export function u_worldPlane(width, height, time){
-  let u_world = m4.translation(0, 120+Math.sin(time*0.2)*2, posRelX + width*0.03); 
-  u_world = m4.xRotate(u_world, degToRad(20)); // Rotazione attorno all'asse X
-  u_world = m4.yRotate(u_world, degToRad(25)); // Rotazione attorno all'asse X
-  u_world = m4.scale(u_world, 1.5,1.5,1.5);
-  u_world = m4.zRotate(u_world, degToRad(-30)); // Applica rotazione attorno all'asse Z
+/**
+ * Calculate the world transformation matrix for the plane.
+ * @param {number} width - Canvas width.
+ * @param {number} height - Canvas height.
+ * @param {number} time - Current time.
+ * @returns {Object} - World transformation matrix for the plane.
+ */
+export function u_worldPlane(width, height, time) {
+  let u_world = m4.translation(0, 120 + Math.sin(time * 0.2) * 2, posRelX + width * 0.03);
+  u_world = m4.xRotate(u_world, degToRad(20));
+  u_world = m4.yRotate(u_world, degToRad(25));
+  u_world = m4.scale(u_world, 1.5, 1.5, 1.5);
+  u_world = m4.zRotate(u_world, degToRad(-30));
   return u_world;
 }
 
-export function u_worldElica(u_world_plane, time){
-  const u_world_elica = m4.xRotate(u_world_plane, -time); // Infine applica la rotazione
-  return u_world_elica;
+/**
+ * Calculate the world transformation matrix for the propeller.
+ * @param {Object} u_world_plane - World transformation matrix of the plane.
+ * @param {number} time - Current time.
+ * @returns {Object} - World transformation matrix for the propeller.
+ */
+export function u_worldElica(u_world_plane, time) {
+  return m4.xRotate(u_world_plane, -time);
 }
 
-export function u_worldFoto(u_world_plane){
-  const prop = 0.5;
-  //let u_world_world = m4.translation(0, -(prop*2.5), -(prop*4)); // Posiziona l'oggetto world in basso
+/**
+ * Calculate the world transformation matrix for the photo.
+ * @param {Object} u_world_plane - World transformation matrix of the plane.
+ * @returns {Object} - World transformation matrix for the photo.
+ */
+export function u_worldFoto(u_world_plane) {
   let u_world = u_world_plane;
-  //u_world = m4.translation(u_world,[0,10,0]);
-  u_world = m4.zRotate(u_world_plane, degToRad(10));
-  //u_world = m4.translation(u_world, 0,0.2,0);
-  /*m4.translation(39, 111, 0); // Posiziona l'oggetto world in basso
-  u_world_world = m4.yRotate(u_world_world, degToRad(-2));
-  u_world_world = m4.xRotate(u_world_world, degToRad(2));
-  u_world_world = m4.scale(u_world_world, prop, prop, prop); // Scala l'oggetto orizzontalmente (asse X)*/
-
-return u_world;
+  u_world = m4.zRotate(u_world, degToRad(10));
+  return u_world;
 }
 
-
-export function u_worldWorld(time){
-    const prop = 5;
-    //let u_world_world = m4.translation(0, -(prop*2.5), -(prop*4)); // Posiziona l'oggetto world in basso
-    let u_world_world = m4.translation(0, -(prop*2.5), -(prop*10)); // Posiziona l'oggetto world in basso
-    //u_world_world = m4.yRotate(u_world_world, 2); // Applica rotazione attorno all'asse Y
-    //u_world_world = m4.scale(u_world_world, 20, 20, 15); // Scala l'oggetto orizzontalmente (asse X)
-    //const fixedRotationAngle = ; // Angolo fisso di 45 gradi
-    u_world_world = m4.xRotate(u_world_world, degToRad(45)); // Applica rotazione fissa attorno all'asse Y
-    u_world_world = m4.yRotate(u_world_world, time/20); // Applica rotazione attorno all'asse Y
-    u_world_world = m4.scale(u_world_world, prop*4, prop*3, prop*4+0.1); // Scala l'oggetto orizzontalmente (asse X)
-
+/**
+ * Calculate the world transformation matrix for the world object.
+ * @param {number} time - Current time.
+ * @returns {Object} - World transformation matrix for the world object.
+ */
+export function u_worldWorld(time) {
+  const prop = 5;
+  let u_world_world = m4.translation(0, -(prop * 2.5), -(prop * 10));
+  u_world_world = m4.xRotate(u_world_world, degToRad(45));
+  u_world_world = m4.yRotate(u_world_world, time / 20);
+  u_world_world = m4.scale(u_world_world, prop * 4, prop * 3, prop * 4 + 0.1);
   return u_world_world;
 }
 
-export function u_worldCloud(time, y, z, scale, rotation){
-  const acc= time*(3.5-scale);
-  let u_world = m4.translation(100-acc, y, z); // Posiziona l'oggetto world in basso
-  u_world = m4.xRotate(u_world, degToRad(rotation)); // Applica rotazione fissa attorno all'asse X
-  u_world = m4.scale(u_world, scale, scale, scale); // Scala l'oggetto orizzontalmente (asse X)
-
-return u_world;
+/**
+ * Calculate the world transformation matrix for a cloud.
+ * @param {number} time - Current time.
+ * @param {number} y - Y position of the cloud.
+ * @param {number} z - Z position of the cloud.
+ * @param {number} scale - Scale of the cloud.
+ * @param {number} rotation - Rotation angle of the cloud.
+ * @returns {Object} - World transformation matrix for the cloud.
+ */
+export function u_worldCloud(time, y, z, scale, rotation) {
+  const acc = time * (3.5 - scale);
+  let u_world = m4.translation(100 - acc, y, z);
+  u_world = m4.xRotate(u_world, degToRad(rotation));
+  u_world = m4.scale(u_world, scale, scale, scale);
+  return u_world;
 }
-
--- js/renderScene.js
+++ js/renderScene.js
@@ -1,136 +1,128 @@
 "use strict";
 
 import { renderCloud, setCloud } from './cloud.js';
-import { checkCollisionObstacle, setListener } from './mousePosition.js';
-import { renderObj, u_worldElica, u_worldFoto, u_worldPlane, u_worldWorld } from './renderObj.js';
+import { setListener } from './mousePosition.js';
+import { renderObj, u_worldElica,  u_worldPlane, u_worldWorld } from './renderObj.js';
 import { renderCoin, renderObstacle, setCoin, setObstacle } from './collectibles.js';
 import { degToRad, isPaused, rand, speed, timing, alphaEnable, zFar, zNear, light } from './utils.js';
 
-  const clouds = [setCloud(rand(4,9),0)];
-  const obstacles = [setObstacle(0)];
-  let coins = [setCoin(0, 0, rand(3,8), rand(45,90))];
-  let i=0;
-
+// Initialize arrays for clouds, obstacles, and coins
+const clouds = [setCloud(rand(4, 9), 0)];
+const obstacles = [setObstacle(0)];
+let coins = [setCoin(0, 0, rand(3, 8), rand(45, 90))];
+let frameCount = 0;
 
 /**
- * Renderizza la scena.
- * @param {WebGLRenderingContext} gl - Contesto WebGL.
- * @param {Object} meshProgramInfo - Informazioni sul programma shader.
- * @param {Array} planeParts - Parti dell'oggetto piano.
- * @param {Array} elicaParts - Parti dell'elica.
- * @param {Array} cameraPosition - Posizione della telecamera.
- * @param {Array} cameraTarget - Punto di mira della telecamera.
- * @param {Array} objOffset - Offset dell'oggetto.
+ * Render the scene.
+ * @param {WebGLRenderingContext} gl - WebGL context.
+ * @param {Object} meshProgramInfo - Shader program information.
+ * @param {Object} parts - Object parts to render.
+ * @param {Array} cameraPosition - Camera position.
+ * @param {Array} cameraTarget - Camera target.
+ * @param {Array} objOffset - Object offset.
  */
 export function renderScene(gl, meshProgramInfo, parts, cameraPosition, cameraTarget, objOffset) {
-  
   setListener(gl);
 
+  /**
+   * Render function called for each animation frame.
+   * @param {number} time - Time in milliseconds.
+   */
   function render(time) {
     if (isPaused) {
       return;
     }
 
-    time *= 0.006;
-    i++;
+    time *= 0.006; // Convert time to seconds
+    frameCount++;
 
-    // Ridimensiona il canvas WebGL alla dimensione dello schermo
+    // Resize WebGL canvas to display size
     webglUtils.resizeCanvasToDisplaySize(gl.canvas);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-    
     gl.enable(gl.DEPTH_TEST);
 
-    if(alphaEnable){
+    if (alphaEnable) {
       gl.enable(gl.BLEND);
       gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
-    }else{
-      gl.disable(gl.BLEND); 
+    } else {
+      gl.disable(gl.BLEND);
     }
 
-
-    // Angolo di campo visivo della telecamera e prospettiva
+    // Set camera field of view and perspective
     const fieldOfViewRadians = degToRad(60);
     const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
     const projection = m4.perspective(fieldOfViewRadians, aspect, zNear, zFar);
 
-    // Posizione della telecamera e vista della scena
+    // Set camera position and view
     const up = [0, 1, 0];
     const camera = m4.lookAt(cameraPosition, cameraTarget, up);
     const view = m4.inverse(camera);
 
-    // Parametri uniformi condivisi per i shader
+    // Shared uniforms for shaders
     const sharedUniforms = {
-      //[60,60,30] -[60,450,450]
-      u_lightDirection: m4.normalize([light[0],light[1],light[2]]), // Direzione della luce
+      u_lightDirection: m4.normalize(light),
       u_view: view,
       u_projection: projection,
-      u_viewWorldPosition: cameraPosition, // Posizione della telecamera nella scena
+      u_viewWorldPosition: cameraPosition,
     };
 
-    // Utilizza il programma shader specificato
+    // Use the specified shader program
     gl.useProgram(meshProgramInfo.program);
     webglUtils.setUniforms(meshProgramInfo, sharedUniforms);
 
+    // Render PLANE
+    let u_world = u_worldPlane(gl.canvas.width, gl.canvas.height, zNear, zFar, time);
+    renderObj(gl, meshProgramInfo, parts.plane, u_world);
 
-    /**AREOPLANO */
-    let u_world = u_worldPlane(gl.canvas.width, gl.canvas.height, zNear, zFar, time); 
-    renderObj(gl,meshProgramInfo, parts.plane, u_world);
-    /**ELICA */
-    renderObj(gl,meshProgramInfo, parts.elica, u_worldElica(u_world, time));
-    /**MONDO */
-    renderObj(gl,meshProgramInfo, parts.world, u_worldWorld(time));
+    // Render ELICA
+    renderObj(gl, meshProgramInfo, parts.elica, u_worldElica(u_world, time));
 
-    /**OSTACOLO */
-    if(i%timing.obstacle==0){
+    // Render WORLD
+    renderObj(gl, meshProgramInfo, parts.world, u_worldWorld(time));
+
+    // Render OBSTACLES periodically
+    if (frameCount % timing.obstacle === 0) {
       obstacles.push(setObstacle(time));
-      if(obstacles.length>5000/(timing.obstacle*speed.obstacle)){
+      if (obstacles.length > 5000 / (timing.obstacle * speed.obstacle)) {
         obstacles.shift();
       }
     }
     obstacles.forEach(data => {
-      renderObstacle(gl,meshProgramInfo, parts.obstacle, time, data);
-    });   
+      renderObstacle(gl, meshProgramInfo, parts.obstacle, time, data);
+    });
 
-    /**MONETE */
-    if(i%timing.coin==0){
-      const n = rand(1,6);
-      const ampiezza = rand(3,8);
-      const yRot = rand(45,90);
-      const y = rand(40,120);
-      for(let i=0; i<n; i++){
-        coins.push(setCoin(time, i, y, ampiezza, yRot));
+    // Render COINS periodically
+    if (frameCount % timing.coin === 0) {
+      const numCoins = rand(1, 6);
+      const amplitude = rand(3, 8);
+      const yRotation = rand(45, 90);
+      const y = rand(40, 120);
+      for (let i = 0; i < numCoins; i++) {
+        coins.push(setCoin(time, i, y, amplitude, yRotation));
       }
-      if(coins.length>30000/(timing.coin*speed.coin)){
+      if (coins.length > 30000 / (timing.coin * speed.coin)) {
         coins.shift();
       }
     }
 
-    const newCoin = [];
-    coins.forEach(data => {
-      const isCollision = renderCoin(gl,meshProgramInfo, parts.coin, time, data);
-      if(!isCollision){
-        newCoin.push(data);
-      }
-    }); 
-    coins=newCoin;
+    coins = coins.filter(data => !renderCoin(gl, meshProgramInfo, parts.coin, time, data));
 
-
-     /** CLOUD */
-    if(i%timing.cloud==0){
-      clouds.push(setCloud(rand(3,9),time));
-      if(clouds.length>5000/(timing.cloud*speed.cloud)){
+    // Render CLOUD periodically
+    if (frameCount % timing.cloud === 0) {
+      clouds.push(setCloud(rand(3, 9), time));
+      if (clouds.length > 5000 / (timing.cloud * speed.cloud)) {
         clouds.shift();
       }
     }
     clouds.forEach(data => {
-      renderCloud(gl,meshProgramInfo, parts.cube, time, data);
-    });    
-    
-    // Richiede il rendering della scena alla prossima animazione frame
+      renderCloud(gl, meshProgramInfo, parts.cube, time, data);
+    });
+
+    // Request the next frame
     requestAnimationFrame(render);
   }
 
-  // Avvia il ciclo di rendering della scena
+  // Start the rendering loop
   requestAnimationFrame(render);
 }
